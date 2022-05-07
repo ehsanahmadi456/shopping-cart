0c70224932212a7c4c90cc2d4ff52990cafe9912
ehsan ahmadi <ehsanahmadi8338@gmail.com>
2022-05-07 23:30:06 +0430

    add folder for get api

---
+++ shopping-cart/src/services/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const BASE_URL = "https://fakestoreapi.com/products";
+
+const getProducts = async () => {
+    const response = await axios.get(`${BASE_URL}/products`);
+    return response.data;
+}
+
+export {getProducts};
