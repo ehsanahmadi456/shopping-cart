8064519699871961486b8a41e0be7a4bb552e4c3
ehsan ahmadi <ehsanahmadi8338@gmail.com>
2022-05-08 01:53:57 +0430

    show products from api on home-page

--- shopping-cart/src/services/api.js
+++ shopping-cart/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://fakestoreapi.com/products";
+const BASE_URL = "https://fakestoreapi.com";
 
 const getProducts = async () => {
     const response = await axios.get(`${BASE_URL}/products`);
